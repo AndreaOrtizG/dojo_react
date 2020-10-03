904ccc9977fa440d8842a925121750fbbc59fbae
Andrea Ortiz <andrea.ortizg9@gmail.com>
2020-10-03 09:42:16 -0500

    last commit

---
+++ src/components/ActionPokemon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ActionPokemon() {
+    return (
+        <div>
+            <button className="btn btn-dark btn-sm mb-2">Comprar Pokemon</button>
+
+            <button className="btn btn-dark btn-sm ">Devolver Pokemon</button>
+        </div>
+    )
+}
---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import {gamesReducer, SearchReducer } from './reducer'
+
+export const defaultGamesState = {
+    pokemon: 20,
+}
+export const initialStateSearch = {
+    loading: false,
+    pokemon: [],
+    error: ''
+}
+
+const rootReducers= combineReducers({
+    games:gamesReducer,
+    seeker:SearchReducer
+})
+
+const store= createStore(rootReducers, applyMiddleware(thunk))
+
+export default store;
